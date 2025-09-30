71057bfa066a0173d6aef73e1381fb091ec80264
Rahul <rahul.wa47@gmail.com>
2025-09-30 21:57:19 +0530

    added domcontroller function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { renderWeatherData } from "./modules/domController";
 import "./styles.css"
 
 console.log("hello world");
@@ -8,6 +9,7 @@ const baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/re
     const response = await fetch(`${baseUrl}${location}?key=${key}&unitGroup=metric`)
     const data = await response.json()
     console.log(data);
+    renderWeatherData(data)
     
  }
 
