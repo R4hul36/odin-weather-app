f82b913117033cc6f96ec9452541ab3dfb4296d3
Rahul <rahul.wa47@gmail.com>
2025-09-30 17:21:14 +0530

    title and search box added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const key = "XPVZZCBV5W42UK53V9SLR9CBY"
 const baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
  async function fetchWeatherData (location) {
-    const response = await fetch(`${baseUrl}${location}?key=${key}`)
-    const {address, latitude, longitude} = await response.json()
-    console.log(longitude);
+    const response = await fetch(`${baseUrl}${location}?key=${key}&unitGroup=metric`)
+    const data = await response.json()
+    console.log(data);
     
  }
 
 
- console.log(fetchWeatherData("london"))
+ console.log(fetchWeatherData("thiruvananthapuram"))
