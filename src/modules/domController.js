c8bc8d86c977863a4cd2c3fa8df26c54f50217c4
Rahul <rahul.wa47@gmail.com>
2025-10-03 18:18:38 +0530

    added code to show correct time for relevant timezone

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -1,37 +1,38 @@
+import { format } from 'date-fns'
 
-import {format} from 'date-fns'
-
-export const renderWeatherData = function ({address, currentConditions:{temp, conditions}, description}, weatherContainer) {
-  weatherContainer.innerHTML = ""
+export const renderWeatherData = function (
+  { address, currentConditions: { temp, conditions }, description, timezone },
+  weatherContainer
+) {
+  weatherContainer.innerHTML = ''
 
   const topSection = document.createElement('div')
-  topSection.classList.add("top-section")
+  topSection.classList.add('top-section')
   const locationName = document.createElement('h2')
-  locationName.textContent = address[0].toUpperCase()+ address.slice(1)
-  const currentTime = document.createElement("span")
-  const formattedTime = format(new Date(), "HH:mm")
+  locationName.textContent = address[0].toUpperCase() + address.slice(1)
+  const currentTime = document.createElement('span')
+  const formattedTime = format(new Date(), 'p', { timezone })
   currentTime.textContent = formattedTime
   topSection.appendChild(locationName)
-  topSection.appendChild(currentTime);
+  topSection.appendChild(currentTime)
 
-  const middleSecction = document.createElement("div")
+  const middleSecction = document.createElement('div')
   middleSecction.classList.add('middle-section')
-  const currWeatherIcon = document.createElement("p")
-  const currTemperature = document.createElement("h1")
-  currTemperature.textContent = temp;
-  const condition = document.createElement("p")
+  const currWeatherIcon = document.createElement('p')
+  const currTemperature = document.createElement('h1')
+  currTemperature.textContent = `${temp}\u00B0C`
+  const condition = document.createElement('p')
   condition.textContent = conditions
   middleSecction.appendChild(currTemperature)
   middleSecction.appendChild(condition)
 
-  const weatherDescription = document.createElement("div")
-  weatherDescription.classList.add("weather-description")
+  const weatherDescription = document.createElement('div')
+  weatherDescription.classList.add('weather-description')
   weatherDescription.textContent = description
-  
+
   // const tempVal = document.createElement("p")
   // tempVal.textContent = temp;
   weatherContainer.appendChild(topSection)
   weatherContainer.appendChild(middleSecction)
   weatherContainer.appendChild(weatherDescription)
-  
 }
