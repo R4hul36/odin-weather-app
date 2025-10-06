cedc3d0c60eb6b73b0618e3d071d295e50bc4b8a
Rahul <rahul.wa47@gmail.com>
2025-10-06 17:31:12 +0530

    added an error module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { renderWeatherData } from './modules/domController'
 import { fetchWeatherData } from './modules/fetchWeatherData'
 import { loadingComponent } from './modules/loading'
+import { errorMessage } from './modules/error'
 import './styles.css'
 
 console.log('hello world')
@@ -8,6 +9,7 @@ console.log('hello world')
 const form = document.querySelector('.form')
 const input = document.querySelector('.search-field')
 const weatherContainer = document.querySelector(".weather-card")
+let currInputValue = ""
 
 input.addEventListener('input', () => {
   input.setCustomValidity('')
@@ -15,18 +17,25 @@ input.addEventListener('input', () => {
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
-  loadingComponent(weatherContainer)
+  if(currInputValue === input.value) {
+    return;
+  }
+  
   if (input.value.trim().length < 3) {
     input.setCustomValidity('Please Enter 3 characters or more')
     input.reportValidity()
     return
   }
+  loadingComponent(weatherContainer)
   const weatherInfo = await fetchWeatherData(input.value)
+  if(!weatherInfo) {
+    errorMessage(weatherContainer)
+    return
+    
+  }
  
   renderWeatherData(weatherInfo, weatherContainer)
-  
- 
-  console.log(weatherInfo);
+  currInputValue = input.value;
   
 })
 
---
+++ src/modules/error.js
@@ -0,0 +1,8 @@
+
+export const errorMessage = function (weatherContainer) {
+    weatherContainer.innerHTML = ""
+    const errorMessage = document.createElement("p")
+    errorMessage.classList.add("error-message")
+    errorMessage.textContent = "Please provide a valid location!"
+    weatherContainer.appendChild(errorMessage)
+}
